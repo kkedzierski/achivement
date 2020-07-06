838394e8fa4dfcb9bdb06d800ebcd8ceddc05ff3
unknown <manzajii@gmail.com>
2020-07-06 18:27:40 +0200

    co robiem

--- achivements.php
+++ achivements.php
@@ -74,7 +74,7 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Dodawanie Osiągnięcia</h5>
+          <h5 class="modal-title" id="exampleModalLongTitle">Dodawanie Osiągnięcia zuęcua czueca zuecua</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
